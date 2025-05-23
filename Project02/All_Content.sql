ddb00503bd849e49a7aaf33f37ab0a02430669c0
brunogsiq <brunogsiq@gmail.com>
2025-05-23 15:11:19 -0300

    new files - Readme.md

--- Project02/All_Content.sql
+++ Project02/All_Content.sql
@@ -1,35 +1,42 @@
 --Comando: SELECT
 	--Buscando todas as colunas/informacoes da tabela Person
-		--SELECT * FROM Person.Person;
+		SELECT * FROM Person.Person;
 
 	--Buscando coluna title na tabela Person
-		--SELECT Title FROM Person.Person;
+		SELECT Title FROM Person.Person;
 
 	--Buscando colunas/informacoes da tabela EmailAddress
-		--SELECT * FROM Person.EmailAddress
+		SELECT * FROM Person.EmailAddress
 
 	--Table: Person
 		--FirstName = Nome 
 		--MiddleName = Sobrenome
 		--LastName = Ultimo 
 
-		--SELECT FirstName, MiddleName, LastName FROM person.Person;
-		--SELECT FirstName FROM Person.Person;
-		--SELECT MiddleName FROM Person.Person;
-		--SELECT LastName FROM Person.Person;
+		SELECT FirstName, MiddleName, LastName FROM person.Person;
+		SELECT FirstName FROM Person.Person;
+		SELECT MiddleName FROM Person.Person;
+		SELECT LastName FROM Person.Person;
 	
 --Comando: DISTINCT
 	--Exemplo, caso nao use o DISTINCT sera apresentada 19K de linhas (rows)
-		--SELECT FirstName FROM Person.Person
+		SELECT FirstName FROM Person.Person
 	--Quando utilizamos o comando corretamente, filtramos e dados duplicados nao serao apresentados
-		--SELECT DISTINCT FirstName FROM Person.Person
+		SELECT DISTINCT FirstName FROM Person.Person
 
 --Comando: Where
 	--SELECT coluna1, coluna2 FROM tabela WHERE condicao
-	--SELECT * FROM Person.Person WHERE LastName = 'miller'
-	--SELECT * FROM Person.Person WHERE LastName = 'miller' AND FirstName = 'anna'
-	--SELECT * FROM Production.Product WHERE color = 'blue' OR color = 'black'
-	--SELECT * FROM Production.Product WHERE ListPrice >= 1500 AND ListPrice <= 2000
+		SELECT * FROM Person.Person WHERE LastName = 'miller'
+		SELECT * FROM Person.Person WHERE LastName = 'miller' AND FirstName = 'anna'
+		SELECT * FROM Production.Product WHERE color = 'blue' OR color = 'black'
+		SELECT * FROM Production.Product WHERE ListPrice >= 1500 AND ListPrice <= 2000
 
-
+--Comando: Count
+	--Número de linhas de acordo com uma condição, Exemplo:
+		--SELECT COUNT (*) FROM TABELA
+		--SELECT COUNT (coluna1) FROM TABELA
+		--SELECT COUNT (DISTINCT coluna1) FROM TABELA
+		SELECT COUNT (*) FROM Person.Person
+		SELECT COUNT (title) FROM Person.Person
+		SELECT COUNT (DISTINCT title) FROM Person.Person
 
--- Project02/Desafios_Exercios/Desafios_Exercicios.sql
+++ Project02/Desafios_Exercios/Desafios_Exercicios.sql
@@ -1,23 +1,19 @@
-/*	DESAFIO 01 = EQUIPE DE PRODUTOS PRECISA DOS NOMES DE TODAS AS PEÇAS QUE PESAM MAIS QUE 500KG PORÉM NÃO MAIS QUE 700KG */
-	--SELECT Name FROM Production.Product WHERE Weight > 500 AND Weight < 700; 
+/*	DESAFIO 01 = EQUIPE DE PRODUTOS PRECISA DOS NOMES DE TODAS AS PEï¿½AS QUE PESAM MAIS QUE 500KG PORï¿½M Nï¿½O MAIS QUE 700KG */
+	SELECT Name FROM Production.Product WHERE Weight > 500 AND Weight < 700; 
 
-/*DESAFIO 02 = Equipe de marketing precisa da relação de empregados que são casados e assalariados*/
-	--SELECT * FROM HumanResources.Employee;
-		--SELECT * FROM HumanResources.Employee WHERE MaritalStatus = 'M' AND SalariedFlag = 1;
-		--SELECT NationalIDNumber FROM HumanResources.Employee WHERE MaritalStatus = 'M' AND SalariedFlag = 1;
+/*DESAFIO 02 = Equipe de marketing precisa da relacao de empregados que sao casados e assalariados*/
+	SELECT * FROM HumanResources.Employee;
+		SELECT * FROM HumanResources.Employee WHERE MaritalStatus = 'M' AND SalariedFlag = 1;
+		SELECT NationalIDNumber FROM HumanResources.Employee WHERE MaritalStatus = 'M' AND SalariedFlag = 1;
 
-/*DESAFIO 03 = User Peter Krebs estaá devendo, pegue o e-mail dele para entrar em contato*/
-	--SELECT BusinessEntityID ,FirstName, LastName FROM Person.Person WHERE FirstName = 'Peter' AND LastName = 'Krebs'
-		--SELECT * FROM Person.EmailAddress WHERE BusinessEntityID = 26
-		--SELECT BusinessEntityID, EmailAddress FROM Person.EmailAddress WHERE BusinessEntityID = 26
+/*DESAFIO 03 = User Peter Krebs estao devendo, pegue o e-mail dele para entrar em contato*/
+	SELECT BusinessEntityID ,FirstName, LastName FROM Person.Person WHERE FirstName = 'Peter' AND LastName = 'Krebs'
+		SELECT * FROM Person.EmailAddress WHERE BusinessEntityID = 26
+		SELECT BusinessEntityID, EmailAddress FROM Person.EmailAddress WHERE BusinessEntityID = 26
 
-/*DESAFIO 04:
-	Quantos Produtos há cadastrados - table: production.product
-	Quantos tamanhos de produtos - table: production.product
-		--SELECT * FROM Production.Product
+/*DESAFIO 04: = Quantos Produtos ha cadastrados - table: production.product e Quantos tamanhos de produtos - table: production.product*/
+	SELECT * FROM Production.Product
 		SELECT COUNT (Name) FROM Production.Product
 		SELECT COUNT (Size) FROM Production.Product
 		SELECT COUNT (DISTINCT Class) FROM Production.Product
-*/
-
 	
